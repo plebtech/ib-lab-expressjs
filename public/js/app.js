1400887e046bbb66522d1154332aac9697c23724
Nick Mc <nbmc@protonmail.com>
2020-03-02 16:38:10 -0600

    completed required portion of lab.

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+let h3 = document.createElement('h3');
+let text = document.createTextNode('behold!');
+h3.append(text);
+document.body.appendChild(h3);
--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const serv = express();
+
+// middleware logger.
+serv.use((req, res, next) => {
+    fs.appendFileSync('log.txt',`${req.url} : ${Date()}\n`);
+    next();
+});
 
 // serv.get('/', (req, res) => {
 //     res.send('Hello from the web server side...');
 // })
-serv.use(express.static(path.join(__dirname, '.../public')));
+serv.use(express.static(path.join(__dirname, '../public')));
 
 serv.listen(3000);
